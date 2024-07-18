4e3b22002c063d4403095e0e8b5493b36c0849ea
Axel Pirek <apirek@pm.me>
2024-07-18 23:38:17 +0200

    Archiviere Input und Output der Tests

--- test_termine.py
+++ test_termine.py
@@ -7,13 +7,18 @@ from bs4 import BeautifulSoup
 import termine
 
 
-def test_termine():
+def test_termine(tmp_path):
     """
     Teste Holen und Parsen von Terminen.
     """
     url = "https://termin.bremen.de/termine/directentry?mdt=4&loc=3&cnc-1705=1"
     soup = termine.get_soup(url)
+    with open(tmp_path / "soup.html", "w") as f:
+        f.write(str(soup))
     appointments = list(termine.parse_appointments(soup))
+    with open(tmp_path / "appointments.txt", "w") as f:
+        for a in appointments:
+            f.write(str(a) + "\n")
     # Ich weiß nicht in welchen Fällen es keine Termine gibt.
     assert len(appointments) > 0
     # Ich vertraue nicht darauf, dass Termine sofort gelöscht werden und unsere Uhren gleich genug gehen.
